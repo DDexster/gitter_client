ee144295d341477c94ed85db357921ff313620ec
DDexster <d.bondrnko@gmail.com>
2017-03-31 12:26:10 +0300

    initial commit, added dependencies and configuration

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+module.exports = {
+	entry: './app.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	module:{
+		preLoaders: [
+		{
+			test: /\.js$/,
+			loader: 'eslint-loader',
+			exclude: /node_modules/,
+			query: require(path.resolve(__dirname, 'eslint.config.js'))
+		}
+		],
+		loaders: [
+		{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}
+		]
+	}
+}
